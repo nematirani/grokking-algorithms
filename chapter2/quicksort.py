8020f0605b6889d8d67c1b6a150dcb188d95bb7b
nemat irani <nmtir2@gmail.com>
2024-07-15 19:22:31 +0200

    Fix quicksort implementation

--- chapter2/quicksort.py
+++ chapter2/quicksort.py
@@ -6,9 +6,7 @@ def quick_sort(arr):
         return arr
     else:
         pivot = random.choice(arr)
-        less = [i for i in arr if i > pivot]
-        greater = [i for i in arr if i < pivot]
-        return quick_sort(less) + [pivot] + quick_sort(greater)
-
-
-print(quick_sort([1, 2, 4, 1, 9, 0]))
+        less = [i for i in arr if i < pivot]
+        equal = [i for i in arr if i == pivot]
+        greater = [i for i in arr if i > pivot]
+        return quick_sort(less) + equal + quick_sort(greater)
