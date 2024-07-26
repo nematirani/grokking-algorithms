e923f0986a7cb9b88f9d3003217fb3112fcae450
nemat irani <nmtir2@gmail.com>
2024-07-26 19:22:00 +0200

    remove unnecessary else statements

--- chapter2/quicksort.py
+++ chapter2/quicksort.py
@@ -4,9 +4,8 @@ import random
 def quick_sort(arr):
     if len(arr) < 2:
         return arr
-    else:
-        pivot = random.choice(arr)
-        less = [i for i in arr if i < pivot]
-        equal = [i for i in arr if i == pivot]
-        greater = [i for i in arr if i > pivot]
-        return quick_sort(less) + equal + quick_sort(greater)
+    pivot = random.choice(arr)
+    less = [i for i in arr if i < pivot]
+    equal = [i for i in arr if i == pivot]
+    greater = [i for i in arr if i > pivot]
+    return quick_sort(less) + equal + quick_sort(greater)
